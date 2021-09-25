9dd86e24a1deb05f8a8248444fd694c4c5c43963
Tony Slonaker <tony.slonaker@gmail.com>
2021-09-25 16:57:57 -0600

    update index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,36 @@
-// TODO: Include packages needed for this application
+//Packages needed for application
+const generateMarkdown = require('./utilities/generateMarkdown.js');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//Array of questions for user input
+const questions = [
+    {
+        name: 'title',
+        type: 'input',
+        message: 'Project Title: (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please input Project Title')
+                return false;
+            }
+        }
+    },
+    {
+        name: 'github',
+        type: 'input',
+        message: 'Enter your GitHub username: (Required)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username...');
+                return false;
+            }
+        }
+    }
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
